07fd5574a29a69e13a3d20eef3fa2578b1ef174f
twetm967 <tcwetmore@gmail.com>
2015-04-06 20:25:38 -0400

    added zero.c to repo

--- zero.c
+++ zero.c
@@ -1,8 +1,17 @@
 // zero.c
 // Device
 
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "traps.h"
+#include "spinlock.h"
 #include "fs.h"
 #include "file.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
+#include "x86.h"
 
 int
 zeroread(struct inode *ip, char *dst, int n)
