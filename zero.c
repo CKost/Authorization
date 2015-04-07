f82500f57ab83a5b4611b64d29758b46f186e83e
twetm967 <tcwetmore@gmail.com>
2015-04-06 20:08:03 -0400

    zero.c makes

--- zero.c
+++ zero.c
@@ -1,1 +1,29 @@
 // zero.c
+// Device
+
+#include "fs.h"
+#include "file.h"
+
+int
+zeroread(struct inode *ip, char *dst, int n)
+{
+	int i = 0;
+	for (; i < n; ++i)
+	{
+		dst[i] = '\0';
+	}
+  	return dst;
+}
+
+int
+zerowrite(struct inode *ip, char *buf, int n)
+{
+  	return 0;
+}
+
+void
+zeroinit(void)
+{
+  	devsw[DEV_ZERO].write = zerowrite;
+  	devsw[DEV_ZERO].read = zeroread;
+}
