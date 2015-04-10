3353aa9552d6c60ee80d7ab1935b480a9e0f93d7
Seth Twigg <stwig468@csunix.cs.bju.edu>
2015-04-10 14:51:22 -0400

    Finished Implementing whoami

--- whoami.c
+++ whoami.c
@@ -19,8 +19,7 @@ main(void)
    USER users[MAX_LEN];
    int numberOfUsers = 0;
    int currProcUID = 0;
-
-   currProcUID = getuid();
+   int currUser = 0;
 
    // Read all usernames, passwords and userIds into "users" array
    static const char filename[] = "etc-passwd";
@@ -49,8 +48,14 @@ main(void)
         }
 
       fclose(file);
- 
-
-      printf(1, "Current process User: %s\n",users[currProcUID].username );
+      
+      currProcUID = getuid();
+      counter = 0;
+      while(counter < numberOfUsers){
+        if(users[counter].userID == atoi(currProcUID)){
+          currUser = counter;
+        }
+      }
+      printf(1, "Current process User: %s\n",users[currUser].username );
       
 }
