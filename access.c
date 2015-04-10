14f3a5083b5fca6c897c8822c503352c6b87b163
Benjamin R. Matthews <bnmtthews@gmail.com>
2015-04-09 22:31:36 -0400

    Small code changes before big master merge.

--- access.c
+++ access.c
@@ -35,15 +35,15 @@ int
 main(int argc, char **argv)
 {
 	//Takes:
-        //User X (UID)
-        // File Y
-       //  Purpose Z
-        if(argc != 3){ //4 Because call style is: access x y  
-		printf(1,"Requires 2 Args: User X, File Y\n");
-        
-        exit();
-        }
-    
-   uaccess(atoi(argv[1]),argv[2]);
-  exit();
+    //User X (UID)
+    // File Y
+   	//  Purpose Z
+	if(argc != 3){ //4 Because call style is: access x y  
+	printf(1,"Requires 2 Args: User X, File Y\n");
+
+	exit();
+	}
+   	uaccess(atoi(argv[1]),argv[2]);
+  	exit();
+  	return 0; // shouldn't ever reach here but ya never know... it is C...
 }
